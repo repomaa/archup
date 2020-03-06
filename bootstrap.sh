6a7d849e01fc65e6411ebd83d534ba514ece130f
Joakim Repomaa <mail@j.repomaa.com>
2020-03-06 11:36:25 +0100

    allow passing a config.yml to archup.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@ pip install ansible jmespath
 curl https://raw.githubusercontent.com/repomaa/archup/master/playbook.yml -o playbook.yml
 
 echo '#!/bin/sh' > archup.sh
-echo 'ansible-playbook playbook.yml' >> archup.sh
+echo 'config=${1:-config.yml}' >> archup.sh
+echo 'ansible-playbook playbook.yml -e "config=$config"' >> archup.sh
 chmod +x archup.sh
 
 echo 'Bootstrapping done! Install arch by running ./archup.sh'
+echo 'If you already have a config you can run ./archup.sh path/to/config.yml'
